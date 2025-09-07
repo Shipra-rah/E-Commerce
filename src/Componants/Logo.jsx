e8c85ae85246b72227c3f3aa24d4b63219beba76
Shipra <Shiprarah@gmail.com>
2025-09-08 02:08:43 +0530

    fix: updated logo path issue

--- src/Componants/Logo.jsx
+++ src/Componants/Logo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import SX from "../assets/SX_Logo.jpg";
 
 export default function Logo() {
   return (
     <div className=" h-12 object-contain flex items-center justify-center border-yellow-800 ">      
       <div className="h-full rounded-full overflow-hidden flex items-center justify-center shadow-lg bg-white">
         <img
-          src={SX}
+          src="../assets/SX_Logo.jpg"
           alt="Logo"
           className="object-cover w-auto h-full rounded-full"
         />
